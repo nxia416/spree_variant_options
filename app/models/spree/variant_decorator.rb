86cf4c25e6470f20fccea927445a2392939bc236
James Duffy <jamesmartinduffy@gmail.com>
2014-05-07 18:56:45 +0900

    Attempt to convert to won

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,10 +5,13 @@ Spree::Variant.class_eval do
   def to_hash
     #actual_price  = self.price
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
+    current_rate = Spree::CurrencyRate.find_by(:base_currency => self.cost_currency)
+    actual_price  = number_with_delimiter(current_rate.convert_to_won(self.price.to_s))
     {
       :id    => self.id,
       :in_stock => self.in_stock?,
-      :price => self.display_price
+      :price => actual_price
+      #:price => self.display_price
     }
   end
 
