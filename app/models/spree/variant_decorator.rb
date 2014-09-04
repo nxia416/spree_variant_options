ec2357606b88494bbd1f7bced194c9e6d883532c
James Duffy <jamesmartinduffy@gmail.com>
2014-09-04 16:56:39 +0900

    Changed variant price to display currency symbol if enabled

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -3,12 +3,11 @@ Spree::Variant.class_eval do
   include ActionView::Helpers::NumberHelper
 
   def to_hash
-    price_no_decimal = number_with_precision(self.price_in(Spree::Config[:presentation_currency]).amount, precision: 0)
-    comma_seperated_price = number_with_delimiter(price_no_decimal, delimiter: ',')
+    price = self.prices.find_by(currency: Spree::Config[:presentation_currency]).display_price.to_s
     {
       :id    => self.id,
       :in_stock => self.in_stock?,
-      :price => comma_seperated_price
+      :price => price
     }
   end
 
